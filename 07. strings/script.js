79dc985fac4fd0c45b410b5207b06379a493066b
Nihal <nihalsheikh714@gmail.com>
2024-03-24 13:39:52 +0530

    Strings module completed

--- 07. strings/script.js
+++ 07. strings/script.js
@@ -157,3 +157,119 @@ substring.
 */
 console.log("This is endsWith() method");
 console.log(searching.endsWith('HARD!'));
+
+// GETTING A SUBSTRING
+console.log("GETTING A SUBSTRING");
+/*
+slice() method:
+- best method.
+
+- Returns a section of a string.
+
+@param start — The index to the beginning of the 
+specified portion of stringObj.
+
+@param end
+The index to the end of the specified portion of 
+stringObj. The substring includes the characters 
+up to, but not including, the character indicated 
+by end. If this value is not specified, the 
+substring continues to the end of stringObj.
+*/
+//example:
+const sliceString = 'I love Biryani!';
+const sliced = sliceString.slice(6)
+console.log(sliced);
+
+// SPLIT STRING INTO SUBSTRINGS
+console.log("SPLIT STRING INTO SUBSTRINGS");
+/*
+split() method:
+- splits a string into an array of substrings.
+*/
+// example: 01 Single String
+const splitString = 'Nekopara';
+const letters = splitString.split('');
+console.log("Single String Splitted:\n",letters);
+
+//example: 02 Sentence
+// when splitting a sentence, put a space in 
+// split('<single_space>')
+const splitSentence = 'Wake up to Reality';
+const wordSplit = splitSentence.split(' ');
+console.log("Sentence Splitted:\n",wordSplit);
+
+
+// REVERSE A STRING
+console.log('REVERSE A STRING');
+/*
+- reverse only works on an Array
+- To reverse a String, we first need toconvert
+the strign into an array.
+- After the string is converted to array, we can
+use the reverse method.
+- here is how code looks like:
+```
+const <variable_name_01> = <String>
+const <variable_name_02> = <variable_name_01>.split("").reverse().join("");
+console.log(<variable_name_02>);
+```
+*/
+//example:
+const testString = 'Hello'; // reverse will be: olleH
+const reverseString = testString.split("").reverse().join("");
+console.log(reverseString);
+
+
+// REPEAT STRING
+console.log('REPEAT STRING');
+/*
+repeat() method:
+- it repeats the string x number of times.
+*/
+//example:
+const catSays = 'meow';
+const repeatString = catSays.repeat(3);
+console.log(repeatString);
+
+
+// TRIM STRING
+console.log('TRIM STRING');
+/*
+trim() method:
+- we can clean or remove empty spaces from the
+string using this method
+- useful when collecting emails from forms
+*/
+// example:
+const exStrv = '   Trim String example.   ';
+console.log(exStrv);
+
+const trimString = exStrv.trim();
+console.log(trimString);
+
+
+// EXERCISE:
+console.log("EXERCISE");
+
+const studentList = 'Students name are: nihal, omkar, dhaval, rehman, aryan'; //54
+
+// 1. find length of the string:
+const length = studentList.length;
+console.log("01 Ans:\n",length);
+
+// 2. Uppercase the entire string:
+const upperCaseList = studentList.toUpperCase();
+console.log("02 Ans:\n",upperCaseList);
+
+// 3. check whether Nihal is on the list:
+const isRehmanOnList = studentList.includes('rehman');
+console.log("03 Ans:\n",isRehmanOnList);
+
+// 4. Create a substring which contains only names from upperCaseList and not any other word:
+const namesOnly = upperCaseList.slice(18);
+console.log("04 Ans:\n",namesOnly);
+
+// 5. Split the above names list into an array without the comma:
+const namesArray = namesOnly.split(', ');
+console.log("05 Ans:\n", namesArray);
